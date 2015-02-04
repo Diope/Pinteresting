d10b324d112d3513c0579a6bfd0e7579e64ea192
Dion <kalani.pettaway@gmail.com>
2015-02-04 14:45:53 -0500

    Even more styling changes

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,6 @@ class Pin < ActiveRecord::Base
   belongs_to :user
   has_attached_file :image, :styles => { :large => '850x850>', :medium => '300x300>', :thumb => '100x100>'}
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
+  validates :image, presence: true
 end
